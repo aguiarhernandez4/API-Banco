6c86f75374923053d42099246f56308f413e6444
João Pedro <aguiarhernandez4@gmail.com>
2025-11-15 12:03:11 -0300

    remove import

--- src/main/java/com/example/SistemabancarioDitial/model/Client.java
+++ src/main/java/com/example/SistemabancarioDitial/model/Client.java
@@ -13,7 +13,6 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
