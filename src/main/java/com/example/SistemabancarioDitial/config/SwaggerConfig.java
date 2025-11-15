e86f91271eafc4c4f095b81572fdbd86f1c0287a
João Pedro <aguiarhernandez4@gmail.com>
2025-11-15 12:17:31 -0300

    Add Swagger.config

--- src/main/java/com/example/SistemabancarioDitial/config/SwaggerConfig.java
+++ src/main/java/com/example/SistemabancarioDitial/config/SwaggerConfig.java
@@ -1,5 +1,53 @@
 package com.example.SistemabancarioDitial.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
 
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Banco - Sistema Bancário Digital")
+                        .description("Documentação da API para operações bancárias, como criação de contas, transações e extratos.")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("João Pedro")
+                                .email("joaopedro@exemplo.com")
+                                .url("https://github.com/aguiarhernandez4")
+                        )
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")
+                        )
+                )
+
+                // Servidores opcionais
+                .servers(java.util.List.of(
+                        new Server().url("http://localhost:8080").description("Servidor Local"),
+                        new Server().url("https://api.meubanco.com").description("Servidor de Produção")
+                ))
+
+                // Segurança global (se precisar Bearer, JWT etc.)
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
 }
