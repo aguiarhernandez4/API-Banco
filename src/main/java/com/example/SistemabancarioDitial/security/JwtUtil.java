f8faf192124055b2a94322d953aaecd531bd4de9
João Pedro <aguiarhernandez4@gmail.com>
2025-11-16 20:01:24 -0300

    Add JwUtil para autenticação

---
+++ src/main/java/com/example/SistemabancarioDitial/security/JwtUtil.java
@@ -0,0 +1,45 @@
+package com.example.SistemabancarioDitial.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+import java.security.Key;
+
+
+@Component
+public class JwtUtil {
+    private final long EXPIRATION_TIME = 1000 * 60 * 60;
+    private final Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String extractUsername(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isValidToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
