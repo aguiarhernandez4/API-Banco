e75226e646d4a068f55def064202f8a7944841b9
João Pedro <aguiarhernandez4@gmail.com>
2025-11-21 21:01:25 -0300

    AnotaçÂão service

--- src/main/java/com/example/SistemabancarioDitial/service/AuthService.java
+++ src/main/java/com/example/SistemabancarioDitial/service/AuthService.java
@@ -1,5 +1,33 @@
 package com.example.SistemabancarioDitial.service;
 
+import com.example.SistemabancarioDitial.dto.LoginRequest;
+import com.example.SistemabancarioDitial.dto.LoginResponse;
+import com.example.SistemabancarioDitial.model.User;
+import com.example.SistemabancarioDitial.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
 
+    @Autowired
+    private UserRepository userRepo;
+
+    public LoginResponse login(LoginRequest request){
+
+        User user = userRepo.findByUsername(request.getUsername())
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+        
+        if (!user.getPassword().equals(request.getPassword())){
+            throw new RuntimeException("Senha incorreta");
+        }
+
+        String fakeToken = "Token" + user.getPassword() + System.currentTimeMillis();
+
+        return LoginResponse.builder()  
+            .username(user.getUsername())
+            .message("Login realizado com sucesso")
+            .token(fakeToken)
+            .build();
+    }
 }
--- src/main/java/com/example/SistemabancarioDitial/service/ClientService.java
+++ src/main/java/com/example/SistemabancarioDitial/service/ClientService.java
@@ -1,5 +1,31 @@
 package com.example.SistemabancarioDitial.service;
 
+import com.example.SistemabancarioDitial.model.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.SistemabancarioDitial.repository.ClientRepository;
+
+
+@Service
 public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepo;
+
+//    public Client save(Client dto) {
+//        Client client = new Client();
+//        client.setName(dto.getName());
+//        client.setEmail(dto.getEmail());
+//        client.setCpf(dto.getCpf());
+//        client.setEndereco(dto.getEndereco());
+//        client.setTelefone(dto.getTelefone());
+//        client.setDatanascimento(dto.getDatanascimento());
+//        client.setPassword(dto.getPassword());
+//        return clientRepo.save(client);
+//    }
+
+    public Client salvar(Client client) {
+        return clientRepo.save(client);
+    }
 
 }
