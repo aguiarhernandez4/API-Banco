f167f3c074858a37fb6e6942003216e03792a7e7
João Pedro <aguiarhernandez4@gmail.com>
2025-11-30 13:05:52 -0300

    NPequenos ajustes

--- src/main/java/com/example/SistemabancarioDitial/SistemabancarioDitialApplication.java
+++ src/main/java/com/example/SistemabancarioDitial/SistemabancarioDitialApplication.java
@@ -1,9 +1,11 @@
 package com.example.SistemabancarioDitial;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class SistemabancarioDitialApplication {
 
 	public static void main(String[] args) {
