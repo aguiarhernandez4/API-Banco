2d1ea6100ce8469fc491f4b6cbaca970d3f1ba66
João Pedro <aguiarhernandez4@gmail.com>
2025-11-30 13:10:43 -0300

    Ajustes salvamento de dados

--- src/main/java/com/example/SistemabancarioDitial/service/ClientService.java
+++ src/main/java/com/example/SistemabancarioDitial/service/ClientService.java
@@ -1,9 +1,15 @@
 package com.example.SistemabancarioDitial.service;
 
+import com.example.SistemabancarioDitial.dto.ClientDTO;
 import com.example.SistemabancarioDitial.model.Client;
+import com.example.SistemabancarioDitial.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.example.SistemabancarioDitial.repository.ClientRepository;
+import org.springframework.web.client.RestClient;
+
+import javax.sql.DataSource;
 
 
 @Service
@@ -12,20 +18,35 @@ public class ClientService {
     @Autowired
     private ClientRepository clientRepo;
 
-//    public Client save(Client dto) {
-//        Client client = new Client();
-//        client.setName(dto.getName());
-//        client.setEmail(dto.getEmail());
-//        client.setCpf(dto.getCpf());
-//        client.setEndereco(dto.getEndereco());
-//        client.setTelefone(dto.getTelefone());
-//        client.setDatanascimento(dto.getDatanascimento());
-//        client.setPassword(dto.getPassword());
-//        return clientRepo.save(client);
-//    }
+    @Autowired
+    private RestClient.Builder builder;
 
-    public Client salvar(Client client) {
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public Client salvar(ClientDTO dto) {
+
+        String cpfLimpo = dto.getCpf().replaceAll("\\D", "");
+        String telefoneLimpo = dto.getTelefone().replaceAll("\\D", "");
+        User user = new User();
+//        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        Client client = Client.builder()
+                .name(dto.getName())
+                .cpf(cpfLimpo)
+                .email(dto.getEmail())
+                .password(dto.getPassword())
+                .datanascimento(dto.getDatanascimento())
+                .endereco(dto.getEndereco())
+                .telefone(telefoneLimpo)
+                .build();
+
         return clientRepo.save(client);
     }
+
+
 
 }
