fc5fb695b8cfdf1701e9370ec1a173c02372ea23
João Pedro <aguiarhernandez4@gmail.com>
2025-12-04 21:10:43 -0300

    Adicionando funcionalidade de listar clientes e buscar por id

--- src/main/java/com/example/SistemabancarioDitial/controller/ClientController.java
+++ src/main/java/com/example/SistemabancarioDitial/controller/ClientController.java
@@ -7,6 +7,8 @@
     import org.springframework.http.ResponseEntity;
     import org.springframework.web.bind.annotation.*;
 
+    import java.util.List;
+
     @RestController
     @RequestMapping("/clients")
     public class ClientController {
@@ -24,4 +26,16 @@
             clientService.excluirClient(id);
             return ResponseEntity.noContent().build();
         }
+
+        @GetMapping("/listaCLients")
+        public List<Client> listarClientes(){
+            return clientService.listarClientes();
+        }
+
+        @GetMapping("/buscaclientes")
+        public ResponseEntity<Client> buscarPorId(@PathVariable Long id){
+            return ResponseEntity.ok(clientService.buscandoPeloId(id));
+        }
+
+
     }
