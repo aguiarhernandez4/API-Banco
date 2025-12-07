cc766982041665122ee19be849b204ac0d60b9e6
João Pedro <aguiarhernandez4@gmail.com>
2025-12-07 19:33:29 -0300

    Adicionando função de criar e excluir contas e ajustes do maven

--- src/main/java/com/example/SistemabancarioDitial/controller/AccountController.java
+++ src/main/java/com/example/SistemabancarioDitial/controller/AccountController.java
@@ -2,12 +2,14 @@ package com.example.SistemabancarioDitial.controller;
 
 import com.example.SistemabancarioDitial.dto.AccountDTO;
 import com.example.SistemabancarioDitial.model.Account;
+import com.example.SistemabancarioDitial.repository.AccountRepository;
 import com.example.SistemabancarioDitial.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/account")
@@ -16,9 +18,23 @@ public class AccountController {
     @Autowired
     private AccountService accountService;
 
+    @Autowired
+    private AccountRepository accountRepo;
+
+
     @PostMapping("criarconta")
     public Account criarConta(@RequestBody AccountDTO dto){
         return accountService.salvarAccount(dto);
     }
 
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> excluirConta(@PathVariable Long id){
+        accountService.excluirConta(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/ListarContas")
+    public List<Account> listarTodasAsContas(){
+        return accountService.listarContas();
+    }
 }
--- src/main/java/com/example/SistemabancarioDitial/service/AccountService.java
+++ src/main/java/com/example/SistemabancarioDitial/service/AccountService.java
@@ -3,9 +3,14 @@ package com.example.SistemabancarioDitial.service;
 import com.example.SistemabancarioDitial.dto.AccountDTO;
 import com.example.SistemabancarioDitial.model.Account;
 import com.example.SistemabancarioDitial.repository.AccountRepository;
+import jakarta.transaction.Transactional;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 @NoArgsConstructor
@@ -19,6 +24,10 @@ public class AccountService {
         AccountDTO accountdto = new AccountDTO();
         accountdto.setSaldo_atual(0.0);
 
+        Random numbercount = new Random();
+        long numeroConta = numbercount.nextLong(100000, 999999);
+
+        dto.setNumbercount(numeroConta);
 
         Account account = Account.builder()
                 .numbercount(dto.getNumbercount())
@@ -30,5 +39,19 @@ public class AccountService {
         return accountRepo.save(account);
     }
 
+    @Transactional
+    public void excluirConta(Long id) {
+        Optional<Account> account = accountRepo.findById(id);
+        if (account.isPresent()) {
+            accountRepo.deleteById(id);
+        } else {
+            throw new RuntimeException("Conta não encontrada");
+        }
+
+    }
+
+    public List<Account> listarContas() {
+        return accountRepo.findAll();
+    }
 
 }
